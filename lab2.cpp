1e4240484ce0375863223ab0b3a2e197d254f8fe
Thegreatqueenhasrisen <elizabeth.u.kwizera2@gmail.com>
2024-09-01 14:39:31 -0500

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+int num1 = 86;
+int num2 = 75;
+int num3 = 62;
+int num4 = 98;
+
+long long int result = static_cast<long long int>(num1) * num2 * num3 * num4;
+cout << "Result" << result << endl;
+
+return 0;
+}
